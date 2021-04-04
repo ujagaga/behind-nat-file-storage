57a5966316d9677a6ad1ad869ce232da044a3148
Rada Berar <ujagaga@gmail.com>
2021-04-04 13:58:36 +0200

    Updated reboot

--- src/web_file_server.c
+++ src/web_file_server.c
@@ -40,6 +40,7 @@ static char ext_response[1024] = {0};
 static char listen_port[7] = {0};
 static time_t last_op_at = 0;
 static char current_path[PATH_MAX] = {0};
+static bool restartFlag = false;
 
 
 void gen_random(char *s) {  
@@ -395,21 +396,14 @@ static void cb(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
       mg_http_printf_chunk(c, "");
       gen_random(admin_user_token);  // Change user token
 
-    }else if (mg_http_match_uri(hm, "/api/pwroff")) {      
-
-      mg_printf(c, "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
-      mg_http_printf_chunk(c, logout_html);
-      mg_http_printf_chunk(c, "");
-      // This might require running the file server as root
-      system("shutdown -P now");
-
     }else if (mg_http_match_uri(hm, "/api/restart")) {      
 
       mg_printf(c, "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
       mg_http_printf_chunk(c, "OK\n");
       mg_http_printf_chunk(c, "");
       sync();
-      system("/usr/sbin/reboot");
+      restartFlag = true;
+      // system("/usr/sbin/reboot");
 
     }else if (mg_http_match_uri(hm, "/api/chk_update")) {      
       
@@ -457,6 +451,10 @@ static void cb(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
     
   }
   (void) fn_data;
+
+  if(restartFlag){
+    system("/usr/sbin/reboot");
+  }
 }
 
 static void usage(const char *prog) {
